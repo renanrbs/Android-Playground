aa799ee9dd7b4770a38a5515997950c7c41cd361
renan <renan.barros@wildlifestudios.com>
2022-03-10 15:50:34 -0300

    update biometry

--- app/src/main/java/com/playground/BiometricActivity.kt
+++ app/src/main/java/com/playground/BiometricActivity.kt
@@ -1,14 +1,14 @@
 package com.playground
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.biometric.BiometricPrompt
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
@@ -16,14 +16,17 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.playground.ui.theme.PlaygroundTheme
 
 class BiometricActivity : AppCompatActivity() {
 
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setupBiometrics()
         setContent {
             PlaygroundTheme {
                 Surface(
@@ -43,14 +46,60 @@ class BiometricActivity : AppCompatActivity() {
         ) {
             OutlinedButton(
                 onClick = ::showBiometry,
-            ){
+            ) {
                 Text(text = "Show biometry")
             }
         }
     }
 
 
-    private fun showBiometry(){
-        Toast.makeText(this,"biometry",Toast.LENGTH_LONG).show()
+    private fun showBiometry() {
+        biometricPrompt.authenticate(promptInfo)
+    }
+
+    private fun setupBiometrics(){
+        val executor = ContextCompat.getMainExecutor(this)
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(
+                    errorCode: Int,
+                    errString: CharSequence
+                ) {
+                    super.onAuthenticationError(errorCode, errString)
+//                    val promptInfo = BiometricPrompt.PromptInfo.Builder()
+//                        .setTitle("PIN login for my app")
+//                        .setSubtitle("Log in using your PIN")
+//                        .setAllowedAuthenticators(DEVICE_CREDENTIAL)
+//                        .build()
+//                    biometricPrompt.authenticate(promptInfo)
+
+                }
+
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult
+                ) {
+                    super.onAuthenticationSucceeded(result)
+                    Toast.makeText(
+                        applicationContext,
+                        "Authentication succeeded!", Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(
+                        applicationContext, "Authentication failed",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+            })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setAllowedAuthenticators(BIOMETRIC_STRONG or DEVICE_CREDENTIAL )
+            .build()
     }
 }
