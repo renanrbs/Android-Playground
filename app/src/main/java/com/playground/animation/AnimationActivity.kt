d0f71b8ff3b4fbeb9f93c92fb47f8ca0531d0f58
renan <renan.barros@wildlifestudios.com>
2022-08-04 16:07:22 -0300

    add animations

--- app/src/main/java/com/playground/animation/AnimationActivity.kt
+++ app/src/main/java/com/playground/animation/AnimationActivity.kt
@@ -3,17 +3,43 @@ package com.playground.animation
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.TargetBasedAnimation
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.withFrameNanos
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.playground.ui.theme.PlaygroundTheme
 
+@OptIn(ExperimentalAnimationApi::class)
 class AnimationActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -28,12 +54,50 @@ class AnimationActivity : AppCompatActivity() {
         }
     }
 
+    @Preview
     @Composable
     fun Screen() {
+        val visible = remember {
+            MutableTransitionState(false).apply {
+                // Start the animation immediately.
+                targetState = true
+            }
+        }
+        var count by remember { mutableStateOf(0) }
+
         Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Animations")
+            Button(onClick = {
+                visible.targetState = !visible.targetState
+                count++
+            }) {
+                Text(text = "Animate")
+            }
+
+            AnimatedVisibility(visibleState = visible,label= "biscoito") {
+                Text(text = "Look at my animations")
+            }
+
+            AnimatedVisibility(
+                visibleState = visible,
+                enter = scaleIn() + fadeIn() ,
+                exit = scaleOut() + fadeOut()
+            ) {
+                Text(text = "No! Look at my animations!")
+            }
+
+            val alpha: Float by animateFloatAsState(if (visible.targetState) 1f else 0.5f)
+            Text(
+                text = "I am animate*AsState", modifier =
+                Modifier.graphicsLayer(alpha = alpha)
+            )
+
+            AnimatedContent(targetState = count) { targetCount ->
+                // Make sure to use `targetCount`, not `count`.
+                Text(text = "You clicked: $targetCount")
+            }
         }
     }
 }
